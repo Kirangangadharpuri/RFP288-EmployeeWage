b1a2ed695e2f4c826c7b06bc468c46638855ca2d
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-07-01 11:05:29 +0530

    [Kiran] Add\ Employees Monthly wage

--- EmployeeWageProblemComp/EmployeeWageProblemComp/EmployeeWage.cs
+++ EmployeeWageProblemComp/EmployeeWageProblemComp/EmployeeWage.cs
@@ -13,30 +13,33 @@ namespace EmployeeWageProblemComp
             int IS_FULL_TIME = 1;
             int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-            int empHrs = 0;
-            int empWage = 0;
+            int Number_Of_Days = 20;
+            int Emp_Hrs = 0;
+            int Emp_Wage = 0;
 
             Random random = new Random();
 
-            int empCheck = random.Next(2);
+            int EmpCheck = random.Next(3);
 
-            if (empCheck == IS_FULL_TIME)
+            if (EmpCheck == IS_FULL_TIME)
             {
-                empHrs = 8;
-                Console.WriteLine("Employee is present");
+                Emp_Hrs = 8;
+                Console.WriteLine("Employee is Present");
+
             }
-            else if (empCheck == IS_PART_TIME)
+            else if (EmpCheck == IS_PART_TIME)
             {
-                empHrs = 4;
-                Console.WriteLine("Employee is working as part time");
+                Emp_Hrs = 4;
+                Console.WriteLine("Employee is Present for Part Time");
             }
             else
             {
-                empHrs = 0;
-                Console.WriteLine("Employee is absent");
+                Emp_Hrs = 0;
+                Console.WriteLine("Employee is Absent");
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage : " + empWage);
+            Emp_Wage = Emp_Hrs * EMP_RATE_PER_HOUR * Number_Of_Days;
+
+            Console.WriteLine("Employee Monthly wage is : " + Emp_Wage + " Rupees");
         }
     }
 }
