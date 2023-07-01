e0b3a34e19d90457db084fcd9701c2e79f916aab
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-07-01 11:01:25 +0530

    [Kiran] Add \ Part time Employee Wage

--- EmployeeWageProblemComp/EmployeeWageProblemComp/EmployeeWage.cs
+++ EmployeeWageProblemComp/EmployeeWageProblemComp/EmployeeWage.cs
@@ -11,6 +11,7 @@ namespace EmployeeWageProblemComp
         public void Employee()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
@@ -23,6 +24,11 @@ namespace EmployeeWageProblemComp
             {
                 empHrs = 8;
                 Console.WriteLine("Employee is present");
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is working as part time");
             }
             else
             {
