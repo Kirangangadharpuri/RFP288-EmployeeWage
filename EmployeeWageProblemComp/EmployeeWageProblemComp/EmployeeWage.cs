6e9c26beec8e7de37f7c5f9fea5d0f43c3691f0a
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-07-01 11:21:11 +0530

    [Kiran] Add \ Employee wage untill contion not satisfied

--- EmployeeWageProblemComp/EmployeeWageProblemComp/EmployeeWage.cs
+++ EmployeeWageProblemComp/EmployeeWageProblemComp/EmployeeWage.cs
@@ -10,33 +10,48 @@ namespace EmployeeWageProblemComp
     {
         public void Employee()
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+            int isFullTime = 1;
+            int isPartTime = 2;
+            int empRatePerHour = 20;
             int empHrs = 0;
             int empWage = 0;
+            int empWagePerMonth = 0;
+            int workingDays = 20;
 
-            Random random = new Random();
+            int totalEmpHrs = 0;
+            int totalWorkingHrs = 100;
+            int totalWorkingDays = 0;
 
-            int empCheck = random.Next(2);
 
-            if (empCheck == IS_FULL_TIME)
+            while (totalEmpHrs <= totalWorkingHrs && totalWorkingDays < workingDays)
             {
-                empHrs = 8;
-                Console.WriteLine("Employee is present");
+                Random random = new Random();
+                int emp_Check = random.Next(3);
+
+                switch (emp_Check)
+                {
+                    case 1:
+                        empHrs = 8;
+                        Console.WriteLine("The Employee is Present");
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        Console.WriteLine("The Employee is Present for Part Time");
+                        break;
+                    default:
+                        empHrs = 0;
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                }
+                totalWorkingDays++;
+                totalEmpHrs = totalEmpHrs + empHrs;
+                empWage = empHrs * empRatePerHour;
+                empWagePerMonth = empWagePerMonth + empWage;
+                Console.WriteLine("TotalWorking days: " + totalWorkingDays);
+                Console.WriteLine("Employee's wage per day is: " + empWage);
+                Console.WriteLine("Total employee's hours are: " + totalEmpHrs);
             }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-                Console.WriteLine("Employee is working as part time");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is absent");
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage : " + empWage);
+            Console.WriteLine("Employee's wages for a Month: " + empWagePerMonth);
         }
     }
 }
